8b92726c561087ce964260caa837885f11dac63c
Shuchita <shuchita@live.com>
2020-02-19 20:43:56 +0000

    Created a registration page and a /register endpoint

--- express_server.js
+++ express_server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cookies = require('cookie-parser');
-
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookies());
+
 
 function generateRandomString() {
   let result = '';
@@ -15,51 +18,29 @@ function generateRandomString() {
   return result;
 }
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookies());
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "idFhed": "http://www.example.com",
-  "GvswhG": "https://www.youtube.com/?tab=w1&gl=CA",
-  "O7QJXg": "https://www.wikipedia.org/",
-  "0fhTa2": "https://9gag.com/",
-  "yHq36q": "https://developer.mozilla.org/en-US/"
+  "GvswhG": "https://www.youtube.com",
+  "O7QJXg": "https://www.wikipedia.org",
+  "0fhTa2": "https://9gag.com",
+  "yHq36q": "https://developer.mozilla.org/en-US"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+// Display all short and long URLs in a table with EDIT and DELETE options
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
+// Display page that allows creation of new short URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
-
+// creates a new short URL and redirects to page showing short and long URL
 app.post("/urls", (req, res) => {
   shortURL = generateRandomString();
   longURL = req.body.longURL;
@@ -67,42 +48,55 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// Displays page that shows short and long URL, with an Edit field
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
+// Redirect to long URL by clicking on the short URL
 app.get(`/u/:shortURL`, (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// Delete a short URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   let key = req.params.shortURL
   delete urlDatabase[key];
   res.redirect("/urls");
 });
 
+// Pressing the "EDIT" button on index page -> redirects to edit page
 app.post("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
+// Submitting edited URL, redirect to index page with new long url
 app.post("/urls/:shortURL/edit", (req, res) => {
   let key = req.params.shortURL;
   urlDatabase[key] = req.body.edit;
   res.redirect("/urls");
 });
 
+// login
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
+// logout
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
+
+// Registration page
+app.get("/register", (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("registration", templateVars);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
